d9d482471b3f23869738a19df062ff22e8d508fb
Shamuel Vargas <63377518+zShamu@users.noreply.github.com>
2024-03-07 18:10:41 -0500

    deployment files v2

--- deploy.sh
+++ deploy.sh
@@ -3,32 +3,16 @@
 # Prompt user for URLs
 read -p "Enter URLs (comma-separated): " urls
 
-# Create services.yaml content
-yaml_content="http:
-  routers:
-    main:
-      entryPoints:
-        - "web"
-      rule: \"Path(`/`\"
-      service: balancer
-
-  services:
-    balancer:
-      loadBalancer:
-        healthCheck:
-            path: /
-            interval: \"10s\"
-            timeout: \"5s\"
-        servers:
-"
-
 # Parse and add URLs to YAML content
 IFS=',' read -ra url_array <<< "$urls"
+yaml_content=""
 for url in "${url_array[@]}"; do
   yaml_content+="        - url: \"$url\"\n"
 done
 
 # Write YAML content to services.yaml file
 echo -e "$yaml_content" > services.yaml
-
 echo "Services generated successfully."
+
+sudo docker-compose up -d
+echo "Load balancer generated successfully."
